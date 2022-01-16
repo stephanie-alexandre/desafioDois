73856ff5c7e249f0f6ef8ba6087eac24611b5c34
stephanie-alexandre <gomesstephanie976@gmail.com>
2022-01-16 19:57:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let age="22";
+
+if (age>18){
+    console.log ("Você tem acesso!")
+};
+
+
+let ofAge="35";
+let human= true;
+
+if (human==true && ofAge>18){
+    console.log ("Prossiga!")
+};
+
+
+
+
+let birthday= "Dezembro";
+if (birthday=="Janeiro"||birthday=="Dezembro"){
+    console.log("Continue!")
+};
+
+
+//*** Exercícios bônus------Não consegui
+//4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+let NameR= "Ricardo";
+
+if (NameR=""){
+    console.log("")
+}
+
+//5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
+
